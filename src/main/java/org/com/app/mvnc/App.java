5df7435812425a72124250db8b62c4921d4875eb
mohan <mohan.chanti4@gmail.com>
2019-07-22 16:00:29 +0530

    new changes

---
+++ src/main/java/org/com/app/mvnc/App.java
@@ -0,0 +1,13 @@
+package org.com.app.mvnc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "mvnc11" );
+    }
+}
